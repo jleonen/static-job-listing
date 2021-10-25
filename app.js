c2965125c359fd70be79ec1beb8e116fd7b7c964
jleonen <jleonen2012@gmail.com>
2021-10-25 01:31:16 -0700

    Added job listing through javascript

--- app.js
+++ app.js
@@ -1,1 +1,211 @@
 "use strict";
+const container = document.querySelector(".job-container");
+// import data from ".data.json";
+
+const data = [
+  {
+    id: 1,
+    company: "Photosnap",
+    logo: "./images/photosnap.svg",
+    new: true,
+    featured: true,
+    position: "Senior Frontend Developer",
+    role: "Frontend",
+    level: "Senior",
+    postedAt: "1d ago",
+    contract: "Full Time",
+    location: "USA Only",
+    languages: ["HTML", "CSS", "JavaScript"],
+    tools: [],
+  },
+  {
+    id: 2,
+    company: "Manage",
+    logo: "./images/manage.svg",
+    new: true,
+    featured: true,
+    position: "Fullstack Developer",
+    role: "Fullstack",
+    level: "Midweight",
+    postedAt: "1d ago",
+    contract: "Part Time",
+    location: "Remote",
+    languages: ["Python"],
+    tools: ["React"],
+  },
+  {
+    id: 3,
+    company: "Account",
+    logo: "./images/account.svg",
+    new: true,
+    featured: false,
+    position: "Junior Frontend Developer",
+    role: "Frontend",
+    level: "Junior",
+    postedAt: "2d ago",
+    contract: "Part Time",
+    location: "USA Only",
+    languages: ["JavaScript"],
+    tools: ["React", "Sass"],
+  },
+  {
+    id: 4,
+    company: "MyHome",
+    logo: "./images/myhome.svg",
+    new: false,
+    featured: false,
+    position: "Junior Frontend Developer",
+    role: "Frontend",
+    level: "Junior",
+    postedAt: "5d ago",
+    contract: "Contract",
+    location: "USA Only",
+    languages: ["CSS", "JavaScript"],
+    tools: [],
+  },
+  {
+    id: 5,
+    company: "Loop Studios",
+    logo: "./images/loop-studios.svg",
+    new: false,
+    featured: false,
+    position: "Software Engineer",
+    role: "Fullstack",
+    level: "Midweight",
+    postedAt: "1w ago",
+    contract: "Full Time",
+    location: "Worldwide",
+    languages: ["JavaScript"],
+    tools: ["Ruby", "Sass"],
+  },
+  {
+    id: 6,
+    company: "FaceIt",
+    logo: "./images/faceit.svg",
+    new: false,
+    featured: false,
+    position: "Junior Backend Developer",
+    role: "Backend",
+    level: "Junior",
+    postedAt: "2w ago",
+    contract: "Full Time",
+    location: "UK Only",
+    languages: ["Ruby"],
+    tools: ["RoR"],
+  },
+  {
+    id: 7,
+    company: "Shortly",
+    logo: "./images/shortly.svg",
+    new: false,
+    featured: false,
+    position: "Junior Developer",
+    role: "Frontend",
+    level: "Junior",
+    postedAt: "2w ago",
+    contract: "Full Time",
+    location: "Worldwide",
+    languages: ["HTML", "JavaScript"],
+    tools: ["Sass"],
+  },
+  {
+    id: 8,
+    company: "Insure",
+    logo: "./images/insure.svg",
+    new: false,
+    featured: false,
+    position: "Junior Frontend Developer",
+    role: "Frontend",
+    level: "Junior",
+    postedAt: "2w ago",
+    contract: "Full Time",
+    location: "USA Only",
+    languages: ["JavaScript"],
+    tools: ["Vue", "Sass"],
+  },
+  {
+    id: 9,
+    company: "Eyecam Co.",
+    logo: "./images/eyecam-co.svg",
+    new: false,
+    featured: false,
+    position: "Full Stack Engineer",
+    role: "Fullstack",
+    level: "Midweight",
+    postedAt: "3w ago",
+    contract: "Full Time",
+    location: "Worldwide",
+    languages: ["JavaScript", "Python"],
+    tools: ["Django"],
+  },
+  {
+    id: 10,
+    company: "The Air Filter Company",
+    logo: "./images/the-air-filter-company.svg",
+    new: false,
+    featured: false,
+    position: "Front-end Dev",
+    role: "Frontend",
+    level: "Junior",
+    postedAt: "1mo ago",
+    contract: "Part Time",
+    location: "Worldwide",
+    languages: ["JavaScript"],
+    tools: ["React", "Sass"],
+  },
+];
+
+const [language1] = data[9].languages;
+data.forEach(function (job) {
+  let html = `
+    <div class="job-listing">
+    <div class="job-info">
+      <div class="company-img">
+        <img src="${job.logo}" alt="Logo" />
+      </div>
+      <div class="job-description">
+        <span class="company">${job.company}</span>
+        ${job.new ? `<span class="new">New!</span>` : ""}
+        ${job.featured ? `<span class="featured">Featured</span>` : ""}
+        <p><strong>${job.position}</strong></p>
+        <div class="extra-info">
+          <span>${job.postedAt} </span>
+          <span>*</span>
+          <span>${job.contract} </span>
+          <span>*</span>
+          <span>${job.location}</span>
+        </div>
+        <!-- <span class="extra-info">  </span> -->
+      </div>
+    </div>
+    <!-- Role -->
+    <div class="tags">
+   
+    <span>${job.role}
+    </span>
+      
+
+      <!-- Level -->
+      <span>${job.level}</span>
+
+      <!-- Languages -->
+     
+      <span>${job.languages[0]}</span>
+      ${job.languages.length > 1 ? `<span>${job.languages[1]}</span>` : ""}
+      ${job.languages.length > 2 ? `<span>${job.languages[2]}</span>` : ""}
+      ${job.tools.length > 0 ? `<span>${job.tools[0]}</span>` : ""}
+      ${job.tools.length > 1 ? `<span>${job.tools[1]}</span>` : ""}
+      ${job.tools.length > 2 ? `<span>${job.tools[2]}</span>` : ""}
+      
+    </div>
+
+  </div>`;
+
+  container.insertAdjacentHTML("beforeend", html);
+});
+
+console.log(data[9].languages.join(","));
+
+{
+  /* <span>${job.languages.join(" ")}</span> */
+}
