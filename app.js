d7a2c2d113d6b0782073983601067e8210c84acc
jleonen <jleonen2012@gmail.com>
2021-10-26 00:37:18 -0700

    Added filter lists to hold filter values

--- app.js
+++ app.js
@@ -6,158 +6,43 @@ const filterBox = document.querySelector(".filter-section");
 const filterTags = document.querySelectorAll(".filter-tags");
 const clearBtn = document.querySelector(".clear-filters");
 
-// const data = [
-//   {
-//     id: 1,
-//     company: "Photosnap",
-//     logo: "./images/photosnap.svg",
-//     new: true,
-//     featured: true,
-//     position: "Senior Frontend Developer",
-//     role: "Frontend",
-//     level: "Senior",
-//     postedAt: "1d ago",
-//     contract: "Full Time",
-//     location: "USA Only",
-//     languages: ["HTML", "CSS", "JavaScript"],
-//     tools: [],
-//   },
-//   {
-//     id: 2,
-//     company: "Manage",
-//     logo: "./images/manage.svg",
-//     new: true,
-//     featured: true,
-//     position: "Fullstack Developer",
-//     role: "Fullstack",
-//     level: "Midweight",
-//     postedAt: "1d ago",
-//     contract: "Part Time",
-//     location: "Remote",
-//     languages: ["Python"],
-//     tools: ["React"],
-//   },
-//   {
-//     id: 3,
-//     company: "Account",
-//     logo: "./images/account.svg",
-//     new: true,
-//     featured: false,
-//     position: "Junior Frontend Developer",
-//     role: "Frontend",
-//     level: "Junior",
-//     postedAt: "2d ago",
-//     contract: "Part Time",
-//     location: "USA Only",
-//     languages: ["JavaScript"],
-//     tools: ["React", "Sass"],
-//   },
-//   {
-//     id: 4,
-//     company: "MyHome",
-//     logo: "./images/myhome.svg",
-//     new: false,
-//     featured: false,
-//     position: "Junior Frontend Developer",
-//     role: "Frontend",
-//     level: "Junior",
-//     postedAt: "5d ago",
-//     contract: "Contract",
-//     location: "USA Only",
-//     languages: ["CSS", "JavaScript"],
-//     tools: [],
-//   },
-//   {
-//     id: 5,
-//     company: "Loop Studios",
-//     logo: "./images/loop-studios.svg",
-//     new: false,
-//     featured: false,
-//     position: "Software Engineer",
-//     role: "Fullstack",
-//     level: "Midweight",
-//     postedAt: "1w ago",
-//     contract: "Full Time",
-//     location: "Worldwide",
-//     languages: ["JavaScript"],
-//     tools: ["Ruby", "Sass"],
-//   },
-//   {
-//     id: 6,
-//     company: "FaceIt",
-//     logo: "./images/faceit.svg",
-//     new: false,
-//     featured: false,
-//     position: "Junior Backend Developer",
-//     role: "Backend",
-//     level: "Junior",
-//     postedAt: "2w ago",
-//     contract: "Full Time",
-//     location: "UK Only",
-//     languages: ["Ruby"],
-//     tools: ["RoR"],
-//   },
-//   {
-//     id: 7,
-//     company: "Shortly",
-//     logo: "./images/shortly.svg",
-//     new: false,
-//     featured: false,
-//     position: "Junior Developer",
-//     role: "Frontend",
-//     level: "Junior",
-//     postedAt: "2w ago",
-//     contract: "Full Time",
-//     location: "Worldwide",
-//     languages: ["HTML", "JavaScript"],
-//     tools: ["Sass"],
-//   },
-//   {
-//     id: 8,
-//     company: "Insure",
-//     logo: "./images/insure.svg",
-//     new: false,
-//     featured: false,
-//     position: "Junior Frontend Developer",
-//     role: "Frontend",
-//     level: "Junior",
-//     postedAt: "2w ago",
-//     contract: "Full Time",
-//     location: "USA Only",
-//     languages: ["JavaScript"],
-//     tools: ["Vue", "Sass"],
-//   },
-//   {
-//     id: 9,
-//     company: "Eyecam Co.",
-//     logo: "./images/eyecam-co.svg",
-//     new: false,
-//     featured: false,
-//     position: "Full Stack Engineer",
-//     role: "Fullstack",
-//     level: "Midweight",
-//     postedAt: "3w ago",
-//     contract: "Full Time",
-//     location: "Worldwide",
-//     languages: ["JavaScript", "Python"],
-//     tools: ["Django"],
-//   },
-//   {
-//     id: 10,
-//     company: "The Air Filter Company",
-//     logo: "./images/the-air-filter-company.svg",
-//     new: false,
-//     featured: false,
-//     position: "Front-end Dev",
-//     role: "Frontend",
-//     level: "Junior",
-//     postedAt: "1mo ago",
-//     contract: "Part Time",
-//     location: "Worldwide",
-//     languages: ["JavaScript"],
-//     tools: ["React", "Sass"],
-//   },
-// ];
+//Language filters
+let languageList = [];
+for (const [key, value] of Object.entries(data)) {
+  value.languages.forEach(function (language) {
+    languageList.push(language);
+  });
+}
+
+const languageFilters = new Set(languageList);
+console.log(languageFilters);
+
+//tool filters
+let toolsList = [];
+for (const [key, value] of Object.entries(data)) {
+  value.tools.forEach(function (tool) {
+    toolsList.push(tool);
+  });
+}
+
+const toolsFilters = new Set(toolsList);
+console.log(toolsFilters);
+
+let roleList = [];
+for (const [key, value] of Object.entries(data)) {
+  roleList.push(value.role);
+}
+
+const roleFilters = new Set(roleList);
+console.log(roleFilters);
+
+let levelList = [];
+for (const [key, value] of Object.entries(data)) {
+  levelList.push(value.level);
+}
+
+const levelFilters = new Set(levelList);
+console.log(levelFilters);
 
 data.forEach(function (job) {
   let html = `
@@ -260,6 +145,8 @@ const clearAll = function () {
 ///////////////////////////////////////
 // let filteredArray = [];
 let filterTag = ["Javascript", "HTML", "Frontend"];
+let filterTag2 = ["JS", "CSS", "Backend"];
+console.log(filterTag.includes(...filterTag2));
 const [data1, data2, data3, data4, data5, data6, data7, data8, data9, data10] =
   data;
 
@@ -288,9 +175,8 @@ const filteredArray = data.filter(
   (company) =>
     // console.log(company.role);
     // filterTag.includes(company.role);
-    filterTag.includes(company.role) &&
-    filterTag.includes(...company.languages) &&
-    filterTag.includes(...company.tools)
+    filterTag.includes(company.role) && filterTag.includes(...company.languages)
+  // filterTag.includes(...company.tools)
 );
 console.log(filteredArray);
 console.log(filterTag.includes(data[0].role));
